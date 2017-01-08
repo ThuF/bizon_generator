3f605f66a4290f877cfad8ffca949377da7ff8f2
Yordan Pavlov <ThuF@users.noreply.github.com>
2017-01-08 15:20:56 +0200

    Update service.js

--- bizon_generator/ScriptingServices/generator/service.js
+++ bizon_generator/ScriptingServices/generator/service.js
@@ -4,42 +4,81 @@
 var response = require('net/http/response');
 var templateUtils = require('generator/utils/templateUtils');
 
-var templateType = 'ScriptingServices';
-var templateName = 'js_database_crud_extended';
-var projectName = 'Students';
-var packageName = 'students';
-var fileName = 'students.js';
+generateDataStructures_table();
 
-var entityName = 'STUDENTS';
-var tableName = 'STUDENTS';
-var tableType = 'table';
-var tableColumns = [{
-		"name":"ID",
-		"type":"INTEGER",
-		"length":"0",
-		"notNull":"true",
-		"primaryKey":"true",
-		"defaultValue":""
-	}, {
-		"name":"NAME",
-		"type":"VARCHAR",
-		"length":"50",
-		"notNull":"false",
-		"primaryKey":"false",
-		"defaultValue":""
-	}, {
-      	"name":"AGE",
-      	"type":"INTEGER",
-      	"length":"0",
-      	"notNull":"false",
-      	"primaryKey":"false",
-      	"defaultValue":""
-      }
-];
+function generateDataStructures_table() {
+	var templateType = 'DataStructures';
+	var templateName = 'table';
+	var projectName = 'Students';
+	var packageName = 'students';
+	var fileName = 'students.table';
+	
+	var columnDefinitions = [{
+			"name":"ID",
+			"type":"INTEGER",
+			"length":"0",
+			"notNull":"true",
+			"primaryKey":"true",
+			"defaultValue":""
+		}, {
+			"name":"NAME",
+			"type":"VARCHAR",
+			"length":"50",
+			"notNull":"false",
+			"primaryKey":"false",
+			"defaultValue":""
+		}, {
+	      	"name":"AGE",
+	      	"type":"INTEGER",
+	      	"length":"0",
+	      	"notNull":"false",
+	      	"primaryKey":"false",
+	      	"defaultValue":""
+	      }
+	];
+	
+	var template = templateUtils.getTemplate(templateType, templateName, projectName, packageName, fileName);
+	templateUtils.addDataStructureTableParameters(template, columnDefinitions);
+	templateUtils.generateTemplate(template);
+}
 
-var template = templateUtils.getTemplate(templateType, templateName, projectName, packageName, fileName);
-templateUtils.addEntityServiceParameters(template, entityName, tableName, tableType, tableColumns);
-templateUtils.generateTemplate(template);
-
+function generateScriptingServices_js_database_crud_extended() {
+	var templateType = 'ScriptingServices';
+	var templateName = 'js_database_crud_extended';
+	var projectName = 'Students';
+	var packageName = 'students';
+	var fileName = 'students.js';
+	
+	var entityName = 'STUDENTS';
+	var tableName = 'STUDENTS';
+	var tableType = 'table';
+	var tableColumns = [{
+			"name":"ID",
+			"type":"INTEGER",
+			"length":"0",
+			"notNull":"true",
+			"primaryKey":"true",
+			"defaultValue":""
+		}, {
+			"name":"NAME",
+			"type":"VARCHAR",
+			"length":"50",
+			"notNull":"false",
+			"primaryKey":"false",
+			"defaultValue":""
+		}, {
+	      	"name":"AGE",
+	      	"type":"INTEGER",
+	      	"length":"0",
+	      	"notNull":"false",
+	      	"primaryKey":"false",
+	      	"defaultValue":""
+	      }
+	];
+	
+	var template = templateUtils.getTemplate(templateType, templateName, projectName, packageName, fileName);
+	templateUtils.addEntityServiceParameters(template, entityName, tableName, tableType, tableColumns);
+	templateUtils.generateTemplate(template);
+}
 response.flush();
 response.close();
