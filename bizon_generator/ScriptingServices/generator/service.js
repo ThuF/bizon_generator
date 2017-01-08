90e243de7323ff998c5daa5590e4adf9de1d46a2
Yordan Pavlov <ThuF@users.noreply.github.com>
2017-01-08 14:30:55 +0200

    Update service.js

--- bizon_generator/ScriptingServices/generator/service.js
+++ bizon_generator/ScriptingServices/generator/service.js
@@ -7,48 +7,71 @@ var repository = require('platform/repository');
 
 var worskapceGenerator = require('generator/workspace');
 
-generateTemplate({
-	'name': 'js_database_crud_extended',
-	'templateParameters': {
-		'fileName': 'students.js',
-		'packageName': 'leo',
-		'fileNameNoExtension': 'students',
-		'entityName': 'STUDENTS',
-		'tableName': 'STUDENTS',
-		'tableType': 'table',
-		'tableColumns': [
-			{
-				"name":"ID",
-				"type":"INTEGER",
-				"length":"0",
-				"notNull":"true",
-				"primaryKey":"true",
-				"defaultValue":""
-			}, {
-				"name":"NAME",
-				"type":"VARCHAR",
-				"length":"50",
-				"notNull":"false",
-				"primaryKey":"false",
-				"defaultValue":""
-			}, {
-		      	"name":"AGE",
-		      	"type":"INTEGER",
-		      	"length":"0",
-		      	"notNull":"false",
-		      	"primaryKey":"false",
-		      	"defaultValue":""
-		      }
-		],
-		'INTEGER': 'INTEGER',
-		'VARCHAR': 'VARCHAR'
-	},
-	'generationParameters': {
-		'projectName': 'Students',
-		'packageName': 'leo',
-		'fileName': 'students'
-	}
-});
+var templateName = 'js_database_crud_extended';
+var projectName = 'Students';
+var packageName = 'students';
+var fileName = 'students.js';
+
+var template = getTemplate(templateName, projectName, packageName, fileName);
+
+var entityName = 'STUDENTS';
+var tableName = 'STUDENTS';
+var tableType = 'table';
+var tableColumns = [{
+	"name":"ID",
+	"type":"INTEGER",
+	"length":"0",
+	"notNull":"true",
+	"primaryKey":"true",
+	"defaultValue":""
+	}, {
+		"name":"NAME",
+		"type":"VARCHAR",
+		"length":"50",
+		"notNull":"false",
+		"primaryKey":"false",
+		"defaultValue":""
+	}, {
+      	"name":"AGE",
+      	"type":"INTEGER",
+      	"length":"0",
+      	"notNull":"false",
+      	"primaryKey":"false",
+      	"defaultValue":""
+      }
+];
+addEntityServiceParameters(template, entityName, tableName, tableType, tableColumns);
+
+generateTemplate(template);
+
+function getTemplate(templateName, projectName, packageName, fileName) {
+	return {
+		'name': templateName,
+		'templateParameters': {
+			'fileName': fileName,
+			'packageName': packageName,
+			'fileNameNoExtension': getFileNameWithoutExtension(fileName),
+		},
+		'generationParameters': {
+			'projectName': projectName,
+			'packageName': packageName,
+			'fileName': getFileNameWithoutExtension(fileName)
+		}
+	};
+}
+
+function getFileNameWithoutExtension(fileName) {
+	return fileName.substring(0, fileName.lastIndexOf('.'));
+}
+
+function addEntityServiceParameters(template, entityName, tableName, tableType, tableColumns) {
+	template.templateParameters.entityName = entityName;
+	template.templateParameters.tableName = tableName;
+	template.templateParameters.tableType = tableType;
+	template.templateParameters.tableColumns = tableColumns;
+	template.templateParameters.INTEGER = 'INTEGER';
+	template.templateParameters.VARCHAR = 'VARCHAR';
+}
 
 function generateTemplate(template) {
 	var templatePath = getScriptingServicesTemplate(template.name);
