6a368b8ebe05d34f926e0a431f4ede0879703801
Yordan Pavlov <ThuF@users.noreply.github.com>
2017-01-09 14:28:11 +0200

    Update service.js

--- bizon_generator/ScriptingServices/generator/service.js
+++ bizon_generator/ScriptingServices/generator/service.js
@@ -6,39 +6,62 @@ var dataStructuresUtils = require('generator/utils/generate/dataStructuresUtils'
 var scriptingServicesUtils = require('generator/utils/generate/scriptingServicesUtils');
 var webContentUtils = require('generator/utils/generate/webContentUtils');
 
-var projectName = 'Humans';
-var packageName = 'humans';
-var fileNameNoExtension = 'humans';
-var serviceFileName = '../../js/' + packageName + '/' + fileNameNoExtension + '.js';
-var pageTitle = 'Humans Home';
+var requestBody = {
+	'projectName': 'Humans',
+	'packageName': 'humans_package',
+	'dataStructures': {
+		'fileName': 'humans.table',
+		'columns': [
+			createDataStructureColumnDefinition('ID', 'INTEGER', undefined, true, true),
+			createDataStructureColumnDefinition('FIRST_NAME', 'VARCHAR', 50, true, undefined),
+			createDataStructureColumnDefinition('LAST_NAME', 'VARCHAR', 50, true, undefined),
+			createDataStructureColumnDefinition('WEIGHT', 'INTEGER', undefined, true, undefined),
+			createDataStructureColumnDefinition('DOB', 'DATE', undefined, true, undefined)
+		]
+	},
+	'scriptingServices': {
+		'fileName': 'humans.js',
+		'tableName': 'HUMANS',
+		'columns': [
+			createScriptingServicesColumnDefinition('ID', 'INTEGER', true),
+			createScriptingServicesColumnDefinition('FIRST_NAME', 'VARCHAR', undefined),
+			createScriptingServicesColumnDefinition('LAST_NAME', 'VARCHAR', undefined),
+			createScriptingServicesColumnDefinition('WEIGHT', 'INTEGER', undefined),
+			createScriptingServicesColumnDefinition('DOB', 'DATE', undefined),
+		]
+	},
+	'webContent': {
+		'fileName': 'humans.html',
+		'pageTitle': 'Humans Home',
+		'serviceFileName': '../../js/humans_package/humans.js',
+		'columns': [
+			createWebContentColumnDefinition('ID', '#', 'integer', true),
+			createWebContentColumnDefinition('FIRST_NAME', 'First Name', undefined, undefined),
+			createWebContentColumnDefinition('LAST_NAME', 'Last Name', undefined, undefined),
+			createWebContentColumnDefinition('WEIGHT', 'Weight (kg)', 'integer', undefined),
+			createWebContentColumnDefinition('DOB', 'Date of Birth', 'date', undefined)
+		]
+	}
+};
 
-var columnDefinitions = [
-	createDataStructureColumnDefinition('ID', 'INTEGER', undefined, true, true),
-	createDataStructureColumnDefinition('FIRST_NAME', 'VARCHAR', 50, true, undefined),
-	createDataStructureColumnDefinition('LAST_NAME', 'VARCHAR', 50, true, undefined),
-	createDataStructureColumnDefinition('WEIGHT', 'INTEGER', undefined, true, undefined),
-	createDataStructureColumnDefinition('DOB', 'DATE', undefined, true, undefined),
-];
-	
-var tableColumns = [
-	createScriptingServicesColumnDefinition('ID', 'INTEGER', true),
-	createScriptingServicesColumnDefinition('FIRST_NAME', 'VARCHAR', undefined),
-	createScriptingServicesColumnDefinition('LAST_NAME', 'VARCHAR', undefined),
-	createScriptingServicesColumnDefinition('WEIGHT', 'INTEGER', undefined),
-	createScriptingServicesColumnDefinition('DOB', 'DATE', undefined),
-];
+var projectName = requestBody.projectName;
+var packageName = requestBody.packageName;
 
-var tableColumnsWeb = [
-	createWebContentColumnDefinition('ID', '#', 'integer', true),
-	createWebContentColumnDefinition('FIRST_NAME', 'First Name', undefined, undefined),
-	createWebContentColumnDefinition('LAST_NAME', 'Last Name', undefined, undefined),
-	createWebContentColumnDefinition('WEIGHT', 'Weight (kg)', 'integer', undefined),
-	createWebContentColumnDefinition('DOB', 'Date of Birth', 'date', undefined)
-];
+var dataStructuresFileName = requestBody.dataStructures.fileName;
+var dataStructuresColumns = requestBody.dataStructures.columns;
 
-dataStructuresUtils.generate(projectName, packageName, fileNameNoExtension + '.table', columnDefinitions);
-scriptingServicesUtils.generate(projectName, packageName, fileNameNoExtension + '.js', fileNameNoExtension.toUpperCase(), tableColumns);
-webContentUtils.generate(projectName, packageName, fileNameNoExtension + '.html', pageTitle, serviceFileName, tableColumnsWeb);
+var scriptingServicesFileName = requestBody.scriptingServices.fileName;
+var scriptingServicesTableName = requestBody.scriptingServices.tableName;
+var scriptingServicesColumns = requestBody.scriptingServices.columns;
+
+var webContentFileName = requestBody.webContent.fileName;
+var webContentPageTitle = requestBody.webContent.pageTitle;
+var webContentServiceFileName = requestBody.webContent.serviceFileName;
+var webContentColumns = requestBody.webContent.columns;
+
+dataStructuresUtils.generate(projectName, packageName, dataStructuresFileName, dataStructuresColumns);
+scriptingServicesUtils.generate(projectName, packageName, scriptingServicesFileName, scriptingServicesTableName, scriptingServicesColumns);
+webContentUtils.generate(projectName, packageName, webContentFileName, webContentPageTitle, webContentServiceFileName, webContentColumns);
 
 response.flush();
 response.close();
