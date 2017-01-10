ed598bdecd00a01a8a10b011fcb0818bb7334456
Yordan Pavlov <ThuF@users.noreply.github.com>
2017-01-10 12:45:23 +0200

    Update scriptingServicesUtils.js

--- bizon_generator/ScriptingServices/generator/utils/generate/scriptingServicesUtils.js
+++ bizon_generator/ScriptingServices/generator/utils/generate/scriptingServicesUtils.js
@@ -6,6 +6,25 @@ var templateUtils = require('generator/utils/templateUtils');
 exports.generate = function(projectName, packageName, fileName, tableName, tableColumns) {
 	var entityName = tableName;
 	var template = templateUtils.getTemplate('ScriptingServices', 'js_database_crud_extended', projectName, packageName, fileName);
-	templateUtils.addEntityServiceParameters(template, entityName, tableName, 'table', tableColumns);
+	addEntityServiceParameters(template, entityName, tableName, 'table', tableColumns);
 	templateUtils.generateTemplate(template);
 };
+
+function addEntityServiceParameters(template, entityName, tableName, tableType, tableColumns) {
+	template.templateParameters.entityName = entityName;
+	template.templateParameters.tableName = tableName;
+	template.templateParameters.tableType = tableType;
+	template.templateParameters.tableColumns = tableColumns;
+
+	// Data Types Mapping
+	template.templateParameters.SMALLINT = 'SMALLINT';
+	template.templateParameters.INTEGER = 'INTEGER';
+	template.templateParameters.BIGINT = 'BIGINT';
+	template.templateParameters.FLOAT = 'FLOAT';
+	template.templateParameters.DOUBLE = 'DOUBLE';
+	template.templateParameters.CHAR = 'CHAR';
+	template.templateParameters.VARCHAR = 'VARCHAR';
+	template.templateParameters.DATE = 'DATE';
+	template.templateParameters.TIME = 'TIME';
+	template.templateParameters.TIMESTAMP = 'TIMESTAMP';
+}
