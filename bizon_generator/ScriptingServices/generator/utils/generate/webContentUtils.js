aef2128ffa563a4e7105372f55092623d0af6abf
Yordan Pavlov <ThuF@users.noreply.github.com>
2017-01-10 12:45:51 +0200

    Update webContentUtils.js

--- bizon_generator/ScriptingServices/generator/utils/generate/webContentUtils.js
+++ bizon_generator/ScriptingServices/generator/utils/generate/webContentUtils.js
@@ -5,6 +5,12 @@ var templateUtils = require('generator/utils/templateUtils');
 
 exports.generate = function(projectName, packageName, fileName, pageTitle, serviceFileName, tableColumns) {
 	var template = templateUtils.getTemplate('WebContentForEntity', 'list_and_manage', projectName, packageName, fileName);
-	templateUtils.addWebContentForEntityParameters(template, pageTitle, serviceFileName, tableColumns);
+	addWebContentForEntityParameters(template, pageTitle, serviceFileName, tableColumns);
 	templateUtils.generateTemplate(template);
 };
+
+function addWebContentForEntityParameters(template, pageTitle, serviceFileName, tableColumns) {
+	template.templateParameters.pageTitle = pageTitle;
+	template.templateParameters.serviceFileName = serviceFileName;
+	template.templateParameters.tableColumns = tableColumns;
+}
